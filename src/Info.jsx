25d85c30119f8e073def0beef33de7e46d3eae7c
iiRooko <moskall_cody@hotmail.com>
2024-11-13 16:30:24 -0500

    Add: in-line width to svg's

--- src/Info.jsx
+++ src/Info.jsx
@@ -11,11 +11,11 @@ function Info() {
                 </p>
             </div>
             <button>
-                <img src="./src/assets/email-svgrepo-com.svg" alt="small email icon" />
+                <img src="./src/assets/email-svgrepo-com.svg" alt="small email icon" width="20px" />
                 Website
             </button>
             <button>
-                <img src="./src/assets/linkedin-svgrepo-com.svg" alt="small LinkedIn icon" />
+                <img src="./src/assets/linkedin-svgrepo-com.svg" alt="small LinkedIn icon" width="20px" />
                 LinkedIn
             </button>
         </div>
