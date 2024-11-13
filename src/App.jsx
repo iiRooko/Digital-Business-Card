891cd3e6c826ee006dfe8820650bd36af4666ae2
iiRooko <moskall_cody@hotmail.com>
2024-11-13 16:46:48 -0500

    Fix: arranged all components into App.jsx
    I did this so everything is imported into App, then main renders App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './App.css'
+import Info from './Info.jsx'
+import About from './About.jsx'
+import Interest from './Interest.jsx'
 
 function App() {
   return (
     <>
+        <Info />
+        <About />
+        <Interest />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Info from './Info.jsx'
-import About from './About.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Info />
-    <About />
   </StrictMode>,
 )
