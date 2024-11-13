298e288feabf35c1a7cbc1594e0d9fb1c877c439
iiRooko <moskall_cody@hotmail.com>
2024-11-13 16:36:00 -0500

    Add: more elements to About.jsx, exported to main

--- src/About.jsx
+++ src/About.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 function About() {
     return (
         <>
+            <h1>
+                About
+            </h1>
+            <p>
+                a dude doing things, yeah....
+            </p>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Info from './Info.jsx'
+import About from './About.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Info />
+    <About />
   </StrictMode>,
 )
