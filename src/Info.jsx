530ffd783485e7cd7771a9484ea191cd1ac95e44
iiRooko <moskall_cody@hotmail.com>
2024-11-13 14:36:43 -0500

    Add: Info.jsx and some elements within

---
+++ src/Info.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import ''
+
+function Info() {
+  return (
+    <>
+        <div>
+            <img className="info-picture" src="" alt="picture of a handsome young man...me!" />
+            <div className="info-text">
+                <p>
+                  Splunk Engineer
+                </p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Info
