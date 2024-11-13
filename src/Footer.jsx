c6721bfc6858405768cac5c6d6402fb4c6993da2
iiRooko <moskall_cody@hotmail.com>
2024-11-13 16:57:36 -0500

    Add: github svg, in-line width

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <>
+            <img src="./src/assets/github-142-svgrepo-com.svg" alt="small github icon" width="25px" />
         </>
     )
 }
