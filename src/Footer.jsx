3223b8b4ffead4af02b299d918db7d4ab098abbe
iiRooko <moskall_cody@hotmail.com>
2024-11-15 11:17:52 -0500

    Add: className's to div and img

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>
+        <div className="footer-container">
             <a href="https://github.com/iiRooko">
-            <img src="./src/assets/github-142-svgrepo-com.svg" alt="small github icon" width="25px" />
+            <img className="footer-img" src="./src/assets/github-142-svgrepo-com.svg" alt="small github icon" width="25px" />
             </a>
         </div>
     )
