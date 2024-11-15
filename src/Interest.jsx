5ec21daf9d05e2697060c50b2a3b29a0aed5b575
iiRooko <moskall_cody@hotmail.com>
2024-11-15 11:57:35 -0500

    Add: h1, p, and some text

--- src/Interest.jsx
+++ src/Interest.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 function Interest() {
     return (
         <>
+            <h1>
+                Interests
+            </h1>
+            <p>
+                Learning stuff.
+            </p>
         </>
     )
 }
