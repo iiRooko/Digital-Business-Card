566ab759b4b425f48f5b7b2714e8ba84a102ae17
iiRooko <moskall_cody@hotmail.com>
2024-11-18 14:03:29 -0500

    Add: styling to footer section

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 function Footer() {
     return (
         <div className="footer-container">
-            <a href="https://github.com/iiRooko">
-            <img className="footer-img" src="./src/assets/github-142-svgrepo-com.svg" alt="small github icon" width="25px" />
-            </a>
+            <div className="footer-github">
+                <a href="https://github.com/iiRooko">
+                <img className="footer-img" src="./src/assets/github-142-svgrepo-com.svg" alt="small github icon" width="25px" />
+                </a>
+            </div>
         </div>
     )
 }
